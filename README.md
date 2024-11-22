fbf31724b58bbc16e4bf8598a1ae6054f4f5d3a7
minlearnminlearn <minlearn@minlearn.org>
2024-11-22 12:03:03 +0800

    111

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# sstp
-setup sstp
+
+```
+ wget -qO- https://ghproxy.minlearn.org/api/https://github.com/minlearnminlearn/sstp_fixed/raw/refs/heads/main/sstp.sh|bash
+```
+
+cat /var/log/accel-ppp/accel-ppp.log
+
--- sstp.sh
+++ sstp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 MYIP=$(wget -qO- icanhazip.com);
 MYIP2="s/xxxxxxxxx/$MYIP/g";
 NIC=$(ip -o $ANU -4 route show to default | awk '{print $5}');
@@ -18,33 +19,44 @@ elif [[ "$ver" = "10" ]]; then
 yoi=Debian10
 fi
 fi
-mkdir /home/sstp
+
+touch /root/log-install.txt
+mkdir -p /home/sstp /var/lib/premium-script
 touch /home/sstp/sstp_account
 touch /var/lib/premium-script/data-user-sstp
 #detail nama perusahaan
 country=ID
 state=Indonesia
 locality=Jawa-Tengah
-organization= Magelang
+organization=Magelang
 organizationalunit=www.vpninjector.com
-commonname=www.vpninjector.com
+commonname=$MYIP
 email=admin@vpninjector
 
 #install sstp
-apt-get install -y build-essential cmake gcc linux-headers-`uname -r` git libpcre3-dev libssl-dev liblua5.1-0-dev ppp
-git clone https://github.com/accel-ppp/accel-ppp.git /opt/accel-ppp-code
-mkdir /opt/accel-ppp-code/build
+apt-get update
+apt-get install -y ppp pppoe iptables iptables-persistent
+apt-get install -y build-essential cmake gcc linux-headers-`uname -r` git libpcre2-dev libssl-dev liblua5.1-0-dev ppp
+
+wget https://ghproxy.minlearn.org/api/https://github.com/accel-ppp/accel-ppp/archive/refs/heads/master.zip
+unzip -o master.zip -d /opt;mv /opt/accel-ppp-master /opt/accel-ppp-code
+rm -rf master.zip
+
+mkdir -p /opt/accel-ppp-code/build
 cd /opt/accel-ppp-code/build/
-cmake -DBUILD_IPOE_DRIVER=TRUE -DBUILD_VLAN_MON_DRIVER=TRUE -DCMAKE_INSTALL_PREFIX=/usr -DKDIR=/usr/src/linux-headers-`uname -r` -DLUA=TRUE -DCPACK_TYPE=$yoi ..
+cmake -DBUILD_DRIVER=TRUE -DRADIUS=FALSE \
+-DBUILD_IPOE_DRIVER=TRUE -DBUILD_VLAN_MON_DRIVER=TRUE -DCMAKE_INSTALL_PREFIX=/usr -DKDIR=/usr/src/linux-headers-`uname -r` -DLUA=TRUE -DCPACK_TYPE=$yoi ..
 make
-cpack -G DEB
-dpkg -i accel-ppp.deb
-mv /etc/accel-ppp.conf.dist /etc/accel-ppp.conf
-wget -O /etc/accel-ppp.conf "https://raw.githubusercontent.com/zahwanugrah/auto/main/accel.conf"
+make install
+#cpack -G DEB
+#dpkg -i accel-ppp.deb
+
+#mv /etc/accel-ppp.conf.dist /etc/accel-ppp.conf
+wget -O /etc/accel-ppp.conf "https://ghproxy.minlearn.org/api/https://github.com/minlearnminlearn/sstp_fixed/raw/refs/heads/main/accel.conf"
 sed -i $MYIP2 /etc/accel-ppp.conf
 chmod +x /etc/accel-ppp.conf
-systemctl start accel-ppp
-systemctl enable accel-ppp
+#systemctl start accel-ppp
+#systemctl enable accel-ppp
 #gen cert sstp
 cd /home/sstp
 openssl genrsa -out ca.key 4096
@@ -54,17 +66,23 @@ openssl genrsa -out server.key 4096
 openssl req -new -key server.key -out ia.csr \
 -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
 openssl x509 -req -days 3650 -in ia.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
-cp /home/sstp/server.crt /home/vps/public_html/server.crt
+#cp /home/sstp/server.crt /home/vps/public_html/server.crt
 iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 444 -j ACCEPT
 iptables -I INPUT -m state --state NEW -m udp -p udp --dport 444 -j ACCEPT
 iptables-save > /etc/iptables.up.rules
 iptables-restore -t < /etc/iptables.up.rules
 netfilter-persistent save > /dev/null
 netfilter-persistent reload > /dev/null
+
+kill -9 `pidof accel-pppd`
+accel-pppd -d -c /etc/accel-ppp.conf
+kill -9 `pidof python3`
+(cd /home/sstp;python3 -m http.server 81 &)
+
 #input perintah sstp
-wget -O /usr/bin/add-sstp https://raw.githubusercontent.com/zahwanugrah/auto/main/add-sstp.sh && chmod +x /usr/bin/add-sstp
-wget -O /usr/bin/del-sstp https://raw.githubusercontent.com/zahwanugrah/auto/main/del-sstp.sh && chmod +x /usr/bin/del-sstp
-wget -O /usr/bin/cek-sstp https://raw.githubusercontent.com/zahwanugrah/auto/main/cek-sstp.sh && chmod +x /usr/bin/cek-sstp
-wget -O /usr/bin/renew-sstp https://raw.githubusercontent.com/zahwanugrah/auto/main/renew-sstp.sh && chmod +x /usr/bin/renew-sstp
+wget -O /usr/bin/add-sstp https://ghproxy.minlearn.org/api/https://github.com/minlearnminlearn/sstp_fixed/raw/refs/heads/main/add-sstp.sh && chmod +x /usr/bin/add-sstp
+wget -O /usr/bin/del-sstp https://ghproxy.minlearn.org/api/https://github.com/minlearnminlearn/sstp_fixed/raw/refs/heads/main/del-sstp.sh && chmod +x /usr/bin/del-sstp
+wget -O /usr/bin/cek-sstp https://ghproxy.minlearn.org/api/https://github.com/minlearnminlearn/sstp_fixed/raw/refs/heads/main/cek-sstp.sh && chmod +x /usr/bin/cek-sstp
+wget -O /usr/bin/renew-sstp https://ghproxy.minlearn.org/api/https://github.com/minlearnminlearn/sstp_fixed/raw/refs/heads/main/renew-sstp.sh && chmod +x /usr/bin/renew-sstp
 rm -f /root/sstp.sh
 
